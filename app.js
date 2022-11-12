e88dc6ff4f6c80bbc4bcef6326ae0bf9b5711aa9
kavyaMVG <kavya.mvg@gmail.com>
2022-11-12 23:38:13 +0530

    JWT tokens

--- app.js
+++ app.js
@@ -6,11 +6,13 @@ const register = require("./models/register");
 const login = require("./models/login");
 const app = express();
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const PORT = process.env.PORT;
+const jwtSecret = process.env.TOKEN_KEY;
 
+app.use(express.json());
 app.use(bodyParser.json());
-
 app.post("/register", async (req, res) => {
   const { email, password } = req.body;
 
@@ -26,8 +28,11 @@ app.post("/register", async (req, res) => {
     req.body.password = await bcrypt.hash(password, salt);
 
     const userModel = new register.users(req.body);
-    const response = await userModel.save();
-    res.status(201).send({ id: response._id, msg: "Registration successful!" });
+    await userModel.save();
+    const token = jwt.sign({ email }, jwtSecret, {
+      expiresIn: "30s",
+    });
+    res.status(201).send({ msg: "Registration successful!", token });
   } catch (err) {
     console.log(err);
     res.status(400).send({ msg: "Please check your inputs" });
@@ -36,8 +41,27 @@ app.post("/register", async (req, res) => {
 
 app.post("/login", async (req, res) => {
   const { email, password } = req.body;
+  const { authorization } = req.headers;
+
   // TODO: validation needed for scenario where password is not passed in the body
   try {
+    if (!authorization) {
+      res.status(401).send({ msg: "Unauthorized" });
+      return;
+    }
+    jwt.verify(authorization, jwtSecret, (err, user) => {
+      if (err) {
+        req.headers.error = err;
+        return;
+      }
+      req.user = user;
+    });
+
+    if (req.headers.error) {
+      res.status(403).send({ msg: "Error verifying Token" });
+      console.log(req.headers.error);
+      return;
+    }
     const user = await login.login.findOne({
       email: email,
     });
