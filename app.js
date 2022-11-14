c318930faca02266b85a4b3b58023f951dcf2d50
kavyaMVG <kavya.mvg@gmail.com>
2022-11-14 20:29:14 +0530

    router

--- app.js
+++ app.js
@@ -8,15 +8,17 @@ const cors = require("cors");
 const app = express();
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
-
+const { userRegistration } = require("./controller/registration");
 const PORT = process.env.PORT;
 const jwtSecret = process.env.TOKEN_KEY;
+const router = express.Router();
 
 app.use(express.json());
 app.use(bodyParser.json());
 
 app.use(cors({ origin: "*" }));
 
+// router.use("/register", userRegistration);
 app.post("/register", async (req, res) => {
   const { email, password } = req.body;
 
---
+++ controller/registration.js
@@ -0,0 +1,1 @@
+exports.userRegistration = (req, res, cb) => {};
