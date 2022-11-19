006a824073180f033f93f3ceb99f9ade668926a5
kavyaMVG <kavya.mvg@gmail.com>
2022-11-19 13:40:02 +0530

    merge

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -3,20 +3,24 @@ const jwt = require("jsonwebtoken");
 const jwtToken = process.env.TOKEN;
 
 exports.auth = (req, res, next) => {
-  const { authorization } = req.headers;
-  if (!authorization) {
-    res.send({ msg: "Unauthorized" });
-    return;
-  }
-
-  jwt.verify(authorization, jwtToken, (err, user) => {
-    if (err) {
-      req.headers.err = err;
-    }
-    if (req.headers.err) {
-      res.status(401).send({ msg: "Your JWT token might have expired" });
+  try {
+    const { authorization } = req.headers;
+    if (!authorization) {
+      res.send({ msg: "Unauthorized" });
       return;
     }
-  });
-  next();
+    jwt.verify(authorization, jwtToken, (err, user) => {
+      if (err) {
+        req.headers.err = err;
+      }
+      if (req.headers.err) {
+        res.status(401).send({ msg: "Your JWT token might have expired" });
+        return;
+      }
+      req.user = user;
+    });
+    next();
+  } catch (err) {
+    res.status(500).send({ msg: "Error while verifying jwt token" });
+  }
 };
