b48e13d4dbe658636d7b4b73f4909e4d419f05bb
kavyaMVG <kavya.mvg@gmail.com>
2022-11-19 14:32:51 +0530

    Code refactor

--- app.js
+++ app.js
@@ -1,48 +1,41 @@
-const dotenv = require("dotenv").config();
+require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const database = require("./database");
 const register = require("./models/register");
-const login = require("./models/login");
 const cors = require("cors");
 const app = express();
 const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
-const { userRegistration } = require("./controller/registration");
+const userRoutes = require("./controllers/user");
+const chatRoutes = require("./controllers/chat");
 const PORT = process.env.PORT;
-const jwtSecret = process.env.TOKEN_KEY;
 const router = express.Router();
-const { auth } = require("./middleware/auth");
+const { auth, generateToken } = require("./middleware/auth");
 
 app.use(express.json());
 app.use(bodyParser.json());
 
 app.use(cors({ origin: "*" }));
 
-// router.use("/register", userRegistration);
-app.post("/register", async (req, res) => {
-  const { email, password } = req.body;
+router.use("/user", userRoutes);
+router.use("/chat", chatRoutes);
+
+app.post("/register", generateToken, async (req, res) => {
+  const { password } = req.body;
 
   try {
-    const user = await register.users.findOne({
-      email: email,
-    });
-    if (user) {
-      res.status(403).send({ msg: "User already exists" });
-      return;
-    }
     const salt = await bcrypt.genSalt(10);
     req.body.password = await bcrypt.hash(password, salt);
 
     const userModel = new register.users(req.body);
     await userModel.save();
-    const token = jwt.sign({ email }, jwtSecret, {
-      expiresIn: "30s",
-    });
-    res.status(201).send({ msg: "Registration successful!", token });
+
+    res
+      .status(201)
+      .send({ msg: "Registration successful!", token: req.authToken });
   } catch (err) {
     console.log(err);
-    res.status(400).send({ msg: "Please check your inputs" });
+    res.status(500).send({ msg: "Error creating user", err });
   }
 });
 
@@ -56,11 +49,11 @@ app.post("/login", auth, async (req, res) => {
     res.status(200).send({ msg: "Login successful!" });
   } catch (err) {
     console.log(err);
-    res.status(500).send("Server error");
+    res.status(500).send({ msg: "Error logging in", err });
   }
 });
 
 app.listen(PORT, async () => {
-  console.log(`Listening on port:${PORT}`);
+  console.log(`Listening on port: ${PORT}`);
   database.connect();
 });
--- middlewares/auth.js
+++ middlewares/auth.js
@@ -2,11 +2,11 @@ require("dotenv").config();
 const jwt = require("jsonwebtoken");
 const jwtToken = process.env.TOKEN;
 
-exports.auth = (req, res, next) => {
+const auth = (req, res, next) => {
   try {
     const { authorization } = req.headers;
     if (!authorization) {
-      res.send({ msg: "Unauthorized" });
+      res.status(403).send({ msg: "Missing authorization" });
       return;
     }
     jwt.verify(authorization, jwtToken, (err, user) => {
@@ -14,13 +14,39 @@ exports.auth = (req, res, next) => {
         req.headers.err = err;
       }
       if (req.headers.err) {
-        res.status(401).send({ msg: "Your JWT token might have expired" });
+        res.status(401).send({ msg: "Invalid jwt Token" });
         return;
       }
       req.user = user;
     });
     next();
   } catch (err) {
-    res.status(500).send({ msg: "Error while verifying jwt token" });
+    console.log(err);
+    res.status(500).send({ msg: "Error while verifying jwt token", err });
   }
+};
+
+const generateToken = async (req, res, next) => {
+  const { email } = req.body;
+  try {
+    const user = await register.users.findOne({
+      email: email,
+    });
+    if (user) {
+      res.status(403).send({ msg: "User already exists" });
+      return;
+    }
+    const token = jwt.sign({ email }, jwtSecret, {
+      expiresIn: "30s",
+    });
+    req.authToken = token;
+  } catch (err) {
+    console.log(err);
+    res.status(500).send({ msg: "Error generating jwt token", err });
+  }
+};
+
+export default {
+  auth,
+  generateToken,
 };
